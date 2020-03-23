9659681974b39c51dd87c05593567032473509dd
Fanny Bouffier <fanny.bouffier@paygreen.fr>
2020-03-23 10:23:25 +0100

    feature:cleaned code in user model for country and role properties

--- src/models/User.ts
+++ src/models/User.ts
@@ -51,12 +51,15 @@ export class User {
         this.lastname = lastName;
         this.firstname = firstname;
         this.publicname = publicname;
-        role != null ? (this.role = Role[role]) : (this.role = null);
+        if (role != null) {
+            this.role = Role[role];
+        }
         this.username = username;
         this.password = password;
-        country != null
-            ? (this.contact = new Contact(email, Country[country]))
-            : (this.contact = new Contact(email, null));
+        this.contact = new Contact(email);
+        if (country != null) {
+            this.contact.country = Country[country];
+        }
     }
 
     /**
